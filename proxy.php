1bf172419c0d21a82ca1ab4b2f47768daca0a429
brunob <brunobergot@gmail.com>
2013-10-14 12:21:52 +0200

    use new marinetraffic API + fix for rawgithub externals sources

--- proxy.php
+++ proxy.php
@@ -138,7 +138,7 @@
 // Change these configuration options if needed, see above descriptions for info.
 $enable_jsonp    = false;
 $enable_native   = true;
-$valid_url_regex = '/http:\/\/www.marinetraffic.com\//';
+$valid_url_regex = '/http:\/\/(.*).marinetraffic.com\//';
 
 // ############################################################################
 
